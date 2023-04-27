874a04053a62cf673d63c1a0a9b124c1e12720a7
BuildTools <unconfigured@null.spigotmc.org>
2023-04-27 22:14:00 +0800

    QRFragment now displays the Date and Time correctly

--- app/src/main/java/com/fernandez/gatekeep/QRFragment.kt
+++ app/src/main/java/com/fernandez/gatekeep/QRFragment.kt
@@ -2,12 +2,15 @@ package com.fernandez.gatekeep
 
 import android.graphics.BitmapFactory
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -15,15 +18,13 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.storage.FirebaseStorage
 
-/**
- * A simple [Fragment] subclass.
- * Use the [QRFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class QRFragment : Fragment() {
     private lateinit var ivQrCode: ImageView
     private lateinit var auth: FirebaseAuth
     private lateinit var database: FirebaseDatabase
+    private lateinit var rvAttendance: RecyclerView
+    private lateinit var attendanceAdapter: AttendanceAdapter
+    private val attendanceList = mutableListOf<Attendance>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,14 +39,68 @@ class QRFragment : Fragment() {
         ivQrCode = view.findViewById(R.id.ivQrCode)
         auth = FirebaseAuth.getInstance()
         database = FirebaseDatabase.getInstance()
+        rvAttendance = view.findViewById(R.id.rvAttendance)
+        attendanceAdapter = AttendanceAdapter(attendanceList)
+        rvAttendance.adapter = attendanceAdapter
+        rvAttendance.layoutManager = LinearLayoutManager(requireContext())
 
-        val currentUserUid = auth.currentUser?.uid
-        val usersRef = database.getReference("users")
+        val currentUserUid = FirebaseAuth.getInstance().currentUser?.uid
         if (currentUserUid != null) {
-            usersRef.child(currentUserUid).addListenerForSingleValueEvent(object : ValueEventListener {
+            // Retrieve user's name from the 'users' node
+            val userRef = FirebaseDatabase.getInstance().getReference("users")
+                .child(currentUserUid)
+                .child("name")
+            userRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val name = snapshot.getValue(String::class.java)
+                    if (name != null) {
+                        // Display the username in a TextView
+                        val tv1 = view.findViewById<TextView>(R.id.tv1)
+                        tv1.text = "Welcome, $name"
+                        // Retrieve attendance data based on user's name
+                        val attendanceRef = FirebaseDatabase.getInstance().getReference("attendance")
+                            .child(currentUserUid)
+                        attendanceRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                                for (attendanceSnapshot in dataSnapshot.children) {
+                                    val attendanceData = attendanceSnapshot.value as HashMap<*, *>
+                                    val date = attendanceData["date"] as String
+                                    val time = attendanceData["time"] as String
+                                    // Do something with the attendance record
+                                    val attendance = Attendance(name, date, time)
+                                    attendanceList.add(attendance)
+                                }
+                                attendanceAdapter.notifyDataSetChanged()
+                            }
+
+                            override fun onCancelled(databaseError: DatabaseError) {
+                                Toast.makeText(
+                                    requireContext(),
+                                    "Failed to load attendance data: ${databaseError.message}",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        })
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    Toast.makeText(
+                        requireContext(),
+                        "Failed to load user data: ${databaseError.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            })
+        }
+        // Retrieve QR code image from Firebase Storage
+        val currentUserUid2 = auth.currentUser?.uid
+        val usersRef = database.getReference("users")
+        if (currentUserUid2 != null) {
+            usersRef.child(currentUserUid2).addListenerForSingleValueEvent(object : ValueEventListener {
                 override fun onDataChange(dataSnapshot: DataSnapshot) {
                     val storageRef = FirebaseStorage.getInstance().reference
-                    val qrCodeRef = storageRef.child("qr_codes/$currentUserUid.jpg")
+                    val qrCodeRef = storageRef.child("qr_codes/$currentUserUid2.jpg")
                     val ONE_MEGABYTE: Long = 1024 * 1024
                     qrCodeRef.getBytes(ONE_MEGABYTE).addOnSuccessListener { bytes ->
                         val qrCodeBitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
