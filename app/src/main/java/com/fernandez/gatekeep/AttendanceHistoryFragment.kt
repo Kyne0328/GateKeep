6561b4241f1b881cc8bfa0444b2e66fb62488d88
BuildTools <unconfigured@null.spigotmc.org>
2023-05-22 14:02:56 +0800

    added date and time formatting

--- app/src/main/java/com/fernandez/gatekeep/AttendanceHistoryFragment.kt
+++ app/src/main/java/com/fernandez/gatekeep/AttendanceHistoryFragment.kt
@@ -1,6 +1,7 @@
 package com.fernandez.gatekeep
 
 import android.annotation.SuppressLint
+import java.text.SimpleDateFormat
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,13 +14,14 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import java.util.Locale
 
 
 class AttendanceHistoryFragment : Fragment() {
     private lateinit var database: FirebaseDatabase
     private lateinit var rvAttendance: RecyclerView
     private lateinit var attendanceHistoryAdapter: AttendanceHistoryAdapter
-    private val attendanceList = mutableListOf<Attendance>()
+    private val attendanceList = mutableListOf<AttendanceAdmin>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,15 +48,23 @@ class AttendanceHistoryFragment : Fragment() {
                 for (userSnapshot in dataSnapshot.children) {
                     for (attendanceSnapshot in userSnapshot.children) {
                         val attendanceData = attendanceSnapshot.value as HashMap<*, *>
-                        val name = attendanceData["name"] as String
-                        val date = attendanceData["date"] as String
-                        val time = attendanceData["time"] as String
-                        // Do something with the attendance record
-                        val attendance = Attendance(name, date, time)
+                        val name = attendanceData["name"] as? String
+                        val time = attendanceData["time"] as? String
+                        val date = attendanceData["date"] as? String
+                        val grade = attendanceData["grade"] as? String
+                        val section = attendanceData["section"] as? String
+                        if (name == null || date == null || time == null || grade == null || section == null) {
+                            continue
+                        }
+                        // Convert date format
+                        val formattedDate = formatDate(date)
+                        // Convert time format
+                        val formattedTime = formatTime(time)
+                        val attendance = AttendanceAdmin(name, date, time, grade, section, formattedDate, formattedTime)
                         attendanceList.add(attendance)
                     }
                 }
-                val sortedList = attendanceList.sortedWith(compareByDescending<Attendance> { it.date }.thenByDescending { it.time })
+                val sortedList = attendanceList.sortedWith(compareByDescending<AttendanceAdmin> { it.date }.thenByDescending { it.time })
                 attendanceList.clear()
                 attendanceList.addAll(sortedList)
                 attendanceHistoryAdapter.notifyDataSetChanged()
@@ -68,5 +78,18 @@ class AttendanceHistoryFragment : Fragment() {
                 ).show()
             }
         })
+    }
+    private fun formatDate(date: String): String {
+        val inputFormat = SimpleDateFormat("dd-MM-yy", Locale.US)
+        val outputFormat = SimpleDateFormat("MMMM d, yyyy", Locale.US)
+        val inputDate = inputFormat.parse(date)
+        return outputFormat.format(inputDate)
+    }
+
+    private fun formatTime(time: String): String {
+        val inputFormat = SimpleDateFormat("HH:mm:ss", Locale.US)
+        val outputFormat = SimpleDateFormat("h:mm a", Locale.US)
+        val inputTime = inputFormat.parse(time)
+        return outputFormat.format(inputTime)
     }
 }
