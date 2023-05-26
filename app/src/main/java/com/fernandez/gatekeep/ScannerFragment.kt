002a022893db8d481e0b70bb01299c4e6e75b60c
BuildTools <unconfigured@null.spigotmc.org>
2023-05-26 18:07:38 +0800

    Minor fix for crash when scanning wrong types of QrCode

--- app/src/main/java/com/fernandez/gatekeep/ScannerFragment.kt
+++ app/src/main/java/com/fernandez/gatekeep/ScannerFragment.kt
@@ -50,56 +50,58 @@ class ScannerFragment : Fragment(), ZXingScannerView.ResultHandler {
     override fun handleResult(result: Result?) {
         result?.let {
             val qrData = it.text.split(",")
-            val userId = qrData[0]
-            val name = qrData[1]
-            val grade = qrData[2]
-            val section = qrData[3]
-            val time = SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(Date())
-            val date = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(Date())
+            if (qrData.size >= 4) {
+                val userId = qrData[0]
+                val name = qrData[1]
+                val grade = qrData[2]
+                val section = qrData[3]
+                val time = SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(Date())
+                val date = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(Date())
 
-            val dialogBuilder = AlertDialog.Builder(activity)
-            dialogBuilder.setMessage("Allow entry for the student: \n$name?")
-            dialogBuilder.setCancelable(false)
-            dialogBuilder.setPositiveButton("Yes") { _, _ ->
-                // Save data to Firebase Realtime Database
-                val databaseRef = FirebaseDatabase.getInstance().getReference("attendance")
-                val attendanceData = HashMap<String, Any>()
-                attendanceData["date"] = date
-                attendanceData["name"] = name
-                attendanceData["time"] = time
-                attendanceData["grade"] = grade
-                attendanceData["section"] = section
-                databaseRef.child(userId).push().setValue(attendanceData)
-                    .addOnSuccessListener {
-                        Toast.makeText(
-                            activity,
-                            "Attendance recorded for $name",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        Log.d(TAG, "Attendance recorded for $name at $time on $date")
+                val dialogBuilder = AlertDialog.Builder(activity)
+                dialogBuilder.setMessage("Allow entry for the student: \n$name?")
+                dialogBuilder.setCancelable(false)
+                dialogBuilder.setPositiveButton("Yes") { _, _ ->
+                    // Save data to Firebase Realtime Database
+                    val databaseRef = FirebaseDatabase.getInstance().getReference("attendance")
+                    val attendanceData = HashMap<String, Any>()
+                    attendanceData["date"] = date
+                    attendanceData["name"] = name
+                    attendanceData["time"] = time
+                    attendanceData["grade"] = grade
+                    attendanceData["section"] = section
+                    databaseRef.child(userId).push().setValue(attendanceData)
+                        .addOnSuccessListener {
+                            Toast.makeText(
+                                activity,
+                                "Attendance recorded for $name",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            Log.d(TAG, "Attendance recorded for $name at $time on $date")
 
-                        // Resume scanning for more QR codes
-                        scannerView.resumeCameraPreview(this)
-                    }
-                    .addOnFailureListener { exception ->
-                        // Handle any exceptions that occurred
-                        Toast.makeText(
-                            activity,
-                            "Failed to record attendance: ${exception.message}",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        Log.e(TAG, "Failed to record attendance", exception)
+                            // Resume scanning for more QR codes
+                            scannerView.resumeCameraPreview(this)
+                        }
+                        .addOnFailureListener { exception ->
+                            // Handle any exceptions that occurred
+                            Toast.makeText(
+                                activity,
+                                "Failed to record attendance: ${exception.message}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            Log.e(TAG, "Failed to record attendance", exception)
 
-                        // Resume scanning for more QR codes
-                        scannerView.resumeCameraPreview(this)
-                    }
+                            // Resume scanning for more QR codes
+                            scannerView.resumeCameraPreview(this)
+                        }
+                }
+                dialogBuilder.setNegativeButton("Cancel") { dialog, _ ->
+                    dialog.dismiss()
+                    scannerView.resumeCameraPreview(this)
+                }
+                val dialog = dialogBuilder.create()
+                dialog.show()
             }
-            dialogBuilder.setNegativeButton("Cancel") { dialog, _ ->
-                dialog.dismiss()
-                scannerView.resumeCameraPreview(this)
-            }
-            val dialog = dialogBuilder.create()
-            dialog.show()
         }
     }
     companion object {
