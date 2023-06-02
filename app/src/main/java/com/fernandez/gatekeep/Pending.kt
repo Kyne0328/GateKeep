1c2b1c3dae9b3f036b35c53e443c11c39f357595
BuildTools <unconfigured@null.spigotmc.org>
2023-06-02 17:01:50 +0800

    pendingSignUp data class

---
+++ app/src/main/java/com/fernandez/gatekeep/Pending.kt
@@ -0,0 +1,12 @@
+package com.fernandez.gatekeep
+
+data class Pending(
+    var name: String,
+    var section: String,
+    var grade: String,
+    var approved: Boolean,
+    var lrn: String,
+    val userId: String,
+    val fcmToken: String,
+    var rejected: Boolean
+)
